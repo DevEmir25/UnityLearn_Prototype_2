0b30ada86d78646548bce68fd30ce73a7586d0aa
Emir Can Öz <emircanoz0101@gmail.com>
2022-07-03 15:41:35 +0300

    init codes

---
+++ MoveForward.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 40.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
+using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
---
+++ SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] animalPrefabs;
+    private float xSpawnRange = 20.0f;
+    private float spawnPosZ = 20.0f;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
